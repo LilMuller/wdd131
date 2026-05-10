07afeb8609c2cd70ae59e6d253c589ebba17adc6
lilmuller <okonvictor444@gmail.com>
2026-05-10 13:44:58 +0100

    Add initial HTML structure, CSS styles, and JavaScript for dynamic content

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+let today = new Date();
+let year = today.getFullYear();
+
+document.getElementById("current-year").textContent = year;
+
+
+document.getElementById("lastModified").innerHTML = "Last Modification: " + document.lastModified;
